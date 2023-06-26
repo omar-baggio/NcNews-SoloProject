2b63d2ebc642af1d9812e0b749dd3c9f9cfb8cce
omar-baggio <oomarali1000@gmail.com>
2023-06-26 14:42:31 +0100

    Test endpoint GET api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,28 @@
+const db = require("../db/connection");
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET/api/topics", () => {
+  test("status: 200, should respond with an array of topic objects with properties slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics).toHaveLength(3);
+        expect(topics).toBeInstanceOf(Array);
+        topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
