91ab862eb7c80febd6e3b5cde8d8d411643c1eb5
omar-baggio <oomarali1000@gmail.com>
2023-06-26 16:22:23 +0100

    Add GET /api endpoint for retrieving api data

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const request = require("./db/connection");
 const { getAllTopics } = require("./controller/topics.controller");
+const { getApi } = require("./controller/api.controller");
 
 const app = express();
+
+app.get("/api", getApi);
 
 app.get("/api/topics", getAllTopics);
 
