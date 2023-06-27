456ee03dd30b2b82b90f6ef29a95c69d88100bac
omar-baggio <oomarali1000@gmail.com>
2023-06-27 13:17:57 +0100

    Add 400 and 404 error handling for GET /articles/:article_id endpoint

--- app.js
+++ app.js
@@ -16,4 +16,20 @@ app.all("/*", (req, res, next) =>
   res.status(404).send({ message: "page not found" })
 );
 
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "input is not valid" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    next(err);
+  }
+});
+
 module.exports = app;
--- model/articles.model.js
+++ model/articles.model.js
@@ -9,6 +9,12 @@ exports.selectArticleById = (article_id) => {
       [article_id]
     )
     .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({
+          status: 404,
+          message: `article with id: ${article_id} does not exist`,
+        });
+      }
       return rows[0];
     });
 };
