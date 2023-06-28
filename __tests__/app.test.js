4ffca328089e0254892955ddb4acc9774cb28c04
omar-baggio <oomarali1000@gmail.com>
2023-06-28 14:02:29 +0100

    Updated the test for Get api/articles to be sorted by descending order by default

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
 const endpointsFile = require("../endpoints.json");
+require("jest-sorted");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -106,12 +107,13 @@ describe("ERROR: GET /api/articles/:article_id", () => {
 });
 
 describe("GET /api/articles", () => {
-  test.only("status: 200, should responds with an article object, containing the properties author, title, article_id, body, topic, created_at, votes, article_img_url and comment_count", () => {
+  test("status: 200, should responds with an article object, containing the properties author, title, article_id, body, topic, created_at, votes, article_img_url and comment_count", () => {
     return request(app)
       .get(`/api/articles`)
       .expect(200)
       .then(({ body }) => {
         expect(body).toBeInstanceOf(Array);
+        expect(body).toBeSorted({ descending: true });
         body.forEach((article) => {
           expect(article).toHaveProperty("article_id");
           expect(article).toHaveProperty("title", expect.any(String));
